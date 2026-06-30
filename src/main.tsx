a20091f755ae8d947f013e755ea38bc417886e50
iMac <imac@iMacs-iMac.local>
2026-06-30 18:02:20 +0100

    YARD Circuits red design launch - homepage live

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-import Terms from './pages/Terms.tsx'
-import Circuits from './pages/Circuits.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/circuits" element={<Circuits />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
